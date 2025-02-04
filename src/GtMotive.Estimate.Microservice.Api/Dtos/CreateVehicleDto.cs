4688da63b148023d9e86a35610b2a37ac94d2c07
Alejandro Box <alejandro.box@plainconcepts.com>
2025-02-04 17:09:46 +0100

    wip: add vehicle controller

---
+++ src/GtMotive.Estimate.Microservice.Api/Dtos/CreateVehicleDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GtMotive.Estimate.Microservice.Api.Dtos
+{
+    public class CreateVehicleDto
+    {
+        [Required]
+        public string Brand { get; set; }
+
+        [Required]
+        [Range(2000, 2100)]
+        public int ProductionYear { get; set; }
+    }
+}
