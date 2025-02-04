defdd43e4485c8c948cb7730ab09e44283f6c376
Alejandro Box <alejandro.box@plainconcepts.com>
2025-02-04 17:07:52 +0100

    wip: add vehicle repository

---
+++ src/GtMotive.Estimate.Microservice.Domain/Models/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿namespace GtMotive.Estimate.Microservice.Domain.Models
+{
+    /// <summary>
+    /// Vehicle model.
+    /// </summary>
+    public class Vehicle
+    {
+        /// <summary>
+        /// Gets or sets the vehicle identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the vehicle brand.
+        /// </summary>
+        public string Brand { get; set; }
+
+        /// <summary>
+        /// Gets or sets the vehicle production year.
+        /// </summary>
+        public int ProductionYear { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating who rented the vehicle.
+        /// </summary>
+        public int? RentedBy { get; set; }
+    }
+}
---
+++ src/GtMotive.Estimate.Microservice.Infrastructure/SqlServer/SqlServerService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data;
+using System.Reflection;
+using System.Threading.Tasks;
+using GtMotive.Estimate.Microservice.Infrastructure.SqlServer.Settings;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+using Microsoft.SqlServer.Dac;
+using Testcontainers.MsSql;
+
+namespace GtMotive.Estimate.Microservice.Infrastructure.SqlServer
+{
+    /// <summary>
+    /// SQL Server service.
+    /// </summary>
+    public class SqlServerService(IOptions<SqlServerDbSettings> dbSettings)
+    {
+        public IDbConnection SqlServerConnection { get; private set; } = new SqlConnection(dbSettings?.Value.ConnectionString);
+
+        /// <summary>
+        /// Run a SQL Server container with a mock database.
+        /// </summary>
+        /// <param name="dbName">Database name.</param>
+        /// <returns>Mock database connection string.</returns>
+        public static async Task<string> RunMockDatabaseAsync(string dbName = "GtMotive")
+        {
+            var sqlContainer = new MsSqlBuilder()
+                .WithName($"gtmotive-sqlserver-{Guid.NewGuid().ToString()[..5]}")
+                .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+                .WithPortBinding(1434, 1433)
+                .Build();
+            await sqlContainer.StartAsync();
+            var connString = sqlContainer.GetConnectionString();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var bacpacStream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.SqlServer.Mock.{dbName}.bacpac");
+            var bacpac = BacPackage.Load(bacpacStream);
+
+            var dacServices = new DacServices(connString);
+            dacServices.ImportBacpac(bacpac, dbName);
+
+            return connString.Replace("master", dbName, System.StringComparison.InvariantCulture);
+        }
+    }
+}
